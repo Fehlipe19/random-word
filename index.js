4cfa0a7e7846c636a4d3bc904394d3e6f382ee16
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-08-26 22:18:37 -0400

    Add ejs files

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { message: "Hello world!" });
+  res.render("index.ejs", { word: "Hello", definition: "Greeting" });
 });
 
 app.listen(port, () => {
