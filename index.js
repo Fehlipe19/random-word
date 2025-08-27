8aad0403d28b78c440b8a91fff0a20cd79c65282
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-08-27 19:43:40 -0400

    Add home file

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import fs from "node:fs";
+import wordListPath from "word-list";
 
 const app = express();
 const port = 3000;
+const wordArray = fs.readFileSync(wordListPath, "utf8").split("\n");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { word: "Hello", definition: "Greeting" });
+  var numIndex = Math.floor(Math.random() * wordArray.length);
+  res.render("index.ejs", { word: wordArray[numIndex], definition: "Greeting" });
 });
 
 app.listen(port, () => {
